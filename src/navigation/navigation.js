9accd700e78d435945d5185d047f64e9709e21a6
hwrnandez <20220686@ricaldone.edu.sv>
2024-07-15 15:30:02 -0600

    first commit

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -3,8 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import Home from '../screens/Home';
 import Add from '../screens/Add';
-import Login from '../screens/Login';
-import Registro from '../screens/Registro';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,8 +10,6 @@ const Navigation = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-                <Stack.Screen name="Registro" component={Registro} options={{ headerShown: false }} />
                 <Stack.Screen name="Home" component={Home} options={{ title: 'Home' }} />
                 <Stack.Screen name="Add" component={Add}
                     options={{ presentation: 'modal', title: 'Agregar productos' }} />
